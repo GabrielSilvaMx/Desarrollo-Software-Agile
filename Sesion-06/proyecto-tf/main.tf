0cec9ce3281c18cd9931a9fe44f152a14b41e1b4
SuperGabo <71516230+GabrielSilvaMx@users.noreply.github.com>
2022-11-21 23:03:31 -0600

    Update main.tf
    Había un error en la ruta de la carpeta del resource "docker_image" (path) y también se actualizó a la última versión 2.23.0.

--- Sesion-06/proyecto-tf/main.tf
+++ Sesion-06/proyecto-tf/main.tf
@@ -1,28 +1,31 @@
 terraform {
   required_providers {
     docker = {
-      source = "kreuzwerker/docker"
-      version = ">= 2.16.0"
+      source  = "kreuzwerker/docker"
+      version = "2.23.0"
     }
   }
 }
 
 provider "docker" {
-  host = "npipe:////.//pipe//docker_engine" # Comenta esta linea si eres usuario MacOS o Linux
+  host = "npipe:////.//pipe//docker_engine" # Para Windows
+  # host = "unix:///var/run/docker.sock" # Para Linux
 }
 
+# Pulls the image
 resource "docker_image" "microservice" {
-  name   = "microservice"
+  name = "microservice"
   build {
-    path = "../microservice/."
+    path = "microservice/."
     tag  = [
       "microservice:latest"
       ]
   }
 }
 
+# Create a container
 resource "docker_container" "microservice" {
-  image = docker_image.microservice.latest
+  image = docker_image.microservice.image_id
   name  = "microservice-demo"
   ports {
     internal = 5000
@@ -32,4 +35,3 @@ resource "docker_container" "microservice" {
     docker_image.microservice
   ]
 }
-
